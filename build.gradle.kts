b4e7c839366cd060349c816776f577060cf79443
Ashwin M <ashwinnambiarm@gmail.com>
2026-07-23 21:09:12 +0400

    feat: Update Android Gradle plugin version to 8.7.3 for improved functionality

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
 // links libnleengine.so for native unit tests, or a :desktop module once
 // that future phase arrives) be added without restructuring this file.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
